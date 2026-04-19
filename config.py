69181b2b02469812577ba26009297658a9cdfd23
Azizxon Qahhorov <113577721+220536cs14@users.noreply.github.com>
2026-04-19 16:08:40 +0500

    Fix formatting of config.py

--- config.py
+++ config.py
@@ -11,7 +11,6 @@ CHAT_ID = "Telegram_USER_ID"
 DAY = "25"  # kun
 MONTH_NAME = "MART 2026"  # oy va yil calendardagi ko‘rinishi bilan
 
-CHROMEDRIVER_PATH = r"PASTE_CHROMEDRIVER_PATH_ON_YOUR_DISC" # example 
-                             #  "D:\Test\Python\Testbot\InstaTopBot\chromedriver-win32\chromedriver.exe"
+CHROMEDRIVER_PATH = r"PASTE_CHROMEDRIVER_PATH_ON_YOUR_DISC" # example "D:\Test\Python\Testbot\InstaTopBot\chromedriver-win32\chromedriver.exe"
 URL = "https://eticket.railway.uz/"   # agarda sayt domeni o'zgarmasa, bunga tegmang
 # ====================
